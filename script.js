3910669818fd9127cce34f2b707df8ed484bc13d
Jesse Putnam <jessejputnam@gmail.com>
2022-03-04 13:29:40 -0500

    Add basic modal win message

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 TABLE OF CONTENTS:
 #DOM Variables
+#DRY Variables
 #Gameboard
 #Players
 #Gameplay
@@ -13,6 +14,20 @@ TABLE OF CONTENTS:
 /* ################# #DOM Variables ################ */
 const playArea = document.querySelector(".gameboard");
 let playSquares = document.querySelectorAll(".play__square");
+
+/* ################# #DRY Variables ################ */
+const player1Win = function () {
+  gameOver = true;
+  winner = player1.playerName;
+};
+const player2Win = function () {
+  gameOver = true;
+  winner = player2.playerName;
+};
+const playerTie = function () {
+  gameOver = true;
+  winner = "Tie";
+};
 
 /* ################# #Gameboard ################ */
 const Gameboard = () => {
@@ -52,6 +67,121 @@ const Player = (symbol, number) => {
   return { playerSymbol, playerName, playerTurn };
 };
 
+/* ################# #Win Conditions ################ */
+const winScenarios = document.addEventListener("mouseup", () => {
+  // Tie Scenario
+  let tieCheck = newBoard.boardValues.some((value) => {
+    return value === "";
+  });
+  if (tieCheck === false) playerTie();
+
+  // Player 1 victory scenarios
+  // Horizontal
+  if (
+    newBoard.boardValues[0] === "X" &&
+    newBoard.boardValues[1] === "X" &&
+    newBoard.boardValues[2] === "X"
+  )
+    player1Win();
+  if (
+    newBoard.boardValues[3] === "X" &&
+    newBoard.boardValues[4] === "X" &&
+    newBoard.boardValues[5] === "X"
+  )
+    player1Win();
+  if (
+    newBoard.boardValues[6] === "X" &&
+    newBoard.boardValues[7] === "X" &&
+    newBoard.boardValues[8] === "X"
+  )
+    player1Win();
+  // Vertical
+  if (
+    newBoard.boardValues[0] === "X" &&
+    newBoard.boardValues[3] === "X" &&
+    newBoard.boardValues[6] === "X"
+  )
+    player1Win();
+  if (
+    newBoard.boardValues[1] === "X" &&
+    newBoard.boardValues[4] === "X" &&
+    newBoard.boardValues[7] === "X"
+  )
+    player1Win();
+  if (
+    newBoard.boardValues[2] === "X" &&
+    newBoard.boardValues[5] === "X" &&
+    newBoard.boardValues[8] === "X"
+  )
+    player1Win();
+  // Diagonal
+  if (
+    newBoard.boardValues[0] === "X" &&
+    newBoard.boardValues[4] === "X" &&
+    newBoard.boardValues[8] === "X"
+  )
+    player1Win();
+  if (
+    newBoard.boardValues[2] === "X" &&
+    newBoard.boardValues[4] === "X" &&
+    newBoard.boardValues[6] === "X"
+  )
+    player1Win();
+
+  // Player 2 victory scenarios
+  // Horizontal
+  if (
+    newBoard.boardValues[0] === "O" &&
+    newBoard.boardValues[1] === "O" &&
+    newBoard.boardValues[2] === "O"
+  )
+    player2Win();
+  if (
+    newBoard.boardValues[3] === "O" &&
+    newBoard.boardValues[4] === "O" &&
+    newBoard.boardValues[5] === "O"
+  )
+    player2Win();
+  if (
+    newBoard.boardValues[6] === "O" &&
+    newBoard.boardValues[7] === "O" &&
+    newBoard.boardValues[8] === "O"
+  )
+    player2Win();
+  // Vertical
+  if (
+    newBoard.boardValues[0] === "O" &&
+    newBoard.boardValues[3] === "O" &&
+    newBoard.boardValues[6] === "O"
+  )
+    player2Win();
+  if (
+    newBoard.boardValues[1] === "O" &&
+    newBoard.boardValues[4] === "O" &&
+    newBoard.boardValues[7] === "O"
+  )
+    player2Win();
+  if (
+    newBoard.boardValues[2] === "O" &&
+    newBoard.boardValues[5] === "O" &&
+    newBoard.boardValues[8] === "O"
+  )
+    player2Win();
+  // Player 2 diagonal victory scenarios
+  if (
+    newBoard.boardValues[0] === "O" &&
+    newBoard.boardValues[4] === "O" &&
+    newBoard.boardValues[8] === "O"
+  )
+    player2Win();
+  if (
+    newBoard.boardValues[2] === "O" &&
+    newBoard.boardValues[4] === "O" &&
+    newBoard.boardValues[6] === "O"
+  )
+    player2Win();
+});
+
 /* ################# #Gameplay ################ */
 const GameFlow = (() => {})();
 
@@ -72,11 +202,14 @@ document.addEventListener("mouseup", () => {
 const player1 = Player("X", 1);
 const player2 = Player("O", 2);
 
+// Play Turn Logic
 playSquares.forEach((square) => {
   square.addEventListener("mousedown", () => {
     let squareNum = square.classList[1][6];
 
+    // Disallow selecting filled box
     if (newBoard.boardValues[squareNum] !== "") return;
+    // Disallow play once game over
     if (gameOver === true) return;
 
     if (player1.playerTurn === true) {
@@ -87,6 +220,7 @@ playSquares.forEach((square) => {
       newBoard.boardValues[squareNum] = player2.playerSymbol;
     }
 
+    // Player turn switch logic
     player1.playerTurn === true
       ? (player1.playerTurn = false)
       : (player1.playerTurn = true);
@@ -95,146 +229,4 @@ playSquares.forEach((square) => {
       ? (player2.playerTurn = false)
       : (player2.playerTurn = true);
   });
-});
-
-document.addEventListener("mouseup", () => {
-  // Player 1 victory scenarios
-  // Horizontal
-  if (
-    newBoard.boardValues[0] === "X" &&
-    newBoard.boardValues[1] === "X" &&
-    newBoard.boardValues[2] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  if (
-    newBoard.boardValues[3] === "X" &&
-    newBoard.boardValues[4] === "X" &&
-    newBoard.boardValues[5] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  if (
-    newBoard.boardValues[6] === "X" &&
-    newBoard.boardValues[7] === "X" &&
-    newBoard.boardValues[8] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  // Vertical
-  if (
-    newBoard.boardValues[0] === "X" &&
-    newBoard.boardValues[3] === "X" &&
-    newBoard.boardValues[6] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  if (
-    newBoard.boardValues[1] === "X" &&
-    newBoard.boardValues[4] === "X" &&
-    newBoard.boardValues[7] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  if (
-    newBoard.boardValues[2] === "X" &&
-    newBoard.boardValues[5] === "X" &&
-    newBoard.boardValues[8] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  // Diagonal
-  if (
-    newBoard.boardValues[0] === "X" &&
-    newBoard.boardValues[4] === "X" &&
-    newBoard.boardValues[8] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-  if (
-    newBoard.boardValues[2] === "X" &&
-    newBoard.boardValues[4] === "X" &&
-    newBoard.boardValues[6] === "X"
-  ) {
-    gameOver = true;
-    winner = player1.playerName;
-  }
-
-  // Player 2 victory scenarios
-  // Horizontal
-  if (
-    newBoard.boardValues[0] === "O" &&
-    newBoard.boardValues[1] === "O" &&
-    newBoard.boardValues[2] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-  if (
-    newBoard.boardValues[3] === "O" &&
-    newBoard.boardValues[4] === "O" &&
-    newBoard.boardValues[5] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-  if (
-    newBoard.boardValues[6] === "O" &&
-    newBoard.boardValues[7] === "O" &&
-    newBoard.boardValues[8] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-
-  // Vertical
-  if (
-    newBoard.boardValues[0] === "O" &&
-    newBoard.boardValues[3] === "O" &&
-    newBoard.boardValues[6] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-  if (
-    newBoard.boardValues[1] === "O" &&
-    newBoard.boardValues[4] === "O" &&
-    newBoard.boardValues[7] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-  if (
-    newBoard.boardValues[2] === "O" &&
-    newBoard.boardValues[5] === "O" &&
-    newBoard.boardValues[8] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-
-  // Player 2 diagonal victory scenarios
-  if (
-    newBoard.boardValues[0] === "O" &&
-    newBoard.boardValues[4] === "O" &&
-    newBoard.boardValues[8] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
-  if (
-    newBoard.boardValues[2] === "O" &&
-    newBoard.boardValues[4] === "O" &&
-    newBoard.boardValues[6] === "O"
-  ) {
-    gameOver = true;
-    winner = player2.playerName;
-  }
 });
